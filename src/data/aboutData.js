f3b029f547c6c124563110e83784b0b7b19794d8
alexiabanos <alexiabanos@gmail.com>
2024-02-24 12:50:20 -0600

    Add demo GIF to README

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,5 +1,6 @@
 export const aboutData = {
     title: "Introductions",
-    description1: "Hello, I'm a Software Developer based in Houston, Texas with a Bachelor of Science degree in Computer Science.",
-    description2: "I'm currently open to new roles as a Frontend, Backend, or Full Stack Stack Developer.",
+    description1: "Hello, I’m a Software Developer based in Houston, Texas, with a Bachelor of Science in Computer Science. Currently, I'm looking for positions as a Frontend, Backend, or Full Stack Developer where I can contribute to meaningful projects and continue to grow professionally.",
+    description2: " I am actively searching for a role that allows me to leverage my knowledge in software development to create impactful solutions. If you’re looking for a committed developer to join your team, I’d love to connect.",
 }
+
